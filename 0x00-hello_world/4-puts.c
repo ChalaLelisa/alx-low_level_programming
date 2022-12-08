18f58e0ae906f1ad7ee9935d3fd5d0ccffdab54b
ChalaLe <clelisa28@gmail.com>
2022-12-08 14:24:03 -0800

    task 4 edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
+
 /**
  *
  * main -Entry point
  *
- * Return: Always o (Success)
+ * Return: 0
  */
-int main (void)
+
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
 
 }
