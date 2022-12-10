326c52dd470b65695d5e58f3cc94f0bef3ecce08
ChalaLe <clelisa28@gmail.com>
2022-12-10 09:45:44 -0800

    task 10

---
+++ 0x00-hello_world/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry in to main
+*printf diffrent combination of two numbers
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 8 && num2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
