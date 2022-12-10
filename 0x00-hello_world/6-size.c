72a30b584a828ff8ac1edab34882e0347f3df27d
ChalaLe <clelisa28@gmail.com>
2022-12-10 05:32:51 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * return: 0
- */
-void main(Void)
+*main - Entry in to main
+*printf size of data type in byte(s)
+*Return: 0 (Success)
+*/
+int main(void)
 {
-	printf("size of a char: %lu byte(s)\n", sizeof(char));
-	printf("size of an int: %lu byte(s)\n", sizeof(int));
-	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of a long long int:%lu byte(s)\n", sizeof(long long int));
-	printf("size of a float:%lu byte(s)\n", sizeof(float));
+	char ch;
+	int int_num;
+	long int long_num;
+	long long long_long_num;
+	float float_num;
+
+	printf("size of a char: %lu byte(s)\n", sizeof(ch));
+	printf("size of an int: %lu byte(s)\n", sizeof(int_num));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long_num));
+	printf("size of a long long int:%lu byte(s)\n", sizeof(long_long_num));
+	printf("size of a float:%lu byte(s)\n", sizeof(float_num));
 return (0);
 }
