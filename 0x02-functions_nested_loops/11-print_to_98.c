75b18bc3e148e51e404b291604ad7a065d99498c
ChalaLe <clelisa28@gmail.com>
2022-12-13 13:38:26 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+include "main"
+include < stdio.h >
+
+/**
+ * print_to_98 - to print numbers from input up to 98
+ * @i, the number to be counted
+ */
+void print_to_98(int n)
+{
+	if ((i >= 98)
+	{
+		while (i > 98)
+			printf("%d, ", i--);
+		printf("%d\n", i);
+	}
+
+	else
+	{
+		while (i < 98)
+			printf("%d, i++");
+
+		printf("%d\n, i);
+	}
+}
