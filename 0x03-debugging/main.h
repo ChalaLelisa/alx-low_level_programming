d446f79ff89474ecdd3d9b362e2e4207d7926ff7
ChalaLe <clelisa28@gmail.com>
2022-12-14 10:41:31 -0800

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void postitive_or_negative(int i);
+int largest_number(int x, int y, int z);
+void print_remaining_days(int month, int day, int year);
+int covert_day(int month, int day);
+
+#indef
+
+
+~                                                                                                   ~
