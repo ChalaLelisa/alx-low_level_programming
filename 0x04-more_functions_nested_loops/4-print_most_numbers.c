ba83a78c6911cab4180e0ff8716b2cc21154fed1
ChalaLe <clelisa28@gmail.com>
2022-12-15 08:38:49 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+/**
+*print_most_numbers - function that prints integers to 9 but not 2&4
+*
+*Return: void
+*/
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
+}
