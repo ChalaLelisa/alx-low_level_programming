708afa632c980c85c1fd6029a9f3726b527bd2e2
ChalaLe <clelisa28@gmail.com>
2022-12-19 10:52:09 -0800

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - A function that Convert a string to an integer.
+ * @s: Char to be checked
+ *
+ * Return: A integer
+ */
+int _atoi(char *s)
+{
+	int c = 0;
+	unsigned int i = 0;
+	int n = 1;
+	int j = 0;
+
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			n *= -1;
+		}
+
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			j = 1;
+			i = (i * 10) + (s[c] - '0');
+			c++;
+		}
+		if (j == 1)
+		{
+			break;
+		}
+
+		c++;
+	}
+
+	i *= n;
+	return (i);
+}
