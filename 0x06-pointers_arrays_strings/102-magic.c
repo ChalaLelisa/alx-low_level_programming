9c66f6bc42cc00cf924078965ea593810fefb0ee
ChalaLe <clelisa28@gmail.com>
2022-12-23 09:31:57 -0800

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
-  * main - Prints an array element
-  *
-  * Return: 0
-  */
+ * main - Prints an array
+ *
+ * Return: Nothing
+ */
 int main(void)
 {
 	int n;
@@ -16,13 +17,13 @@ int main(void)
 /*
  * write your line of code here...
  * Remember:
- * ou are not allowed to use a
+ * you are not allowed to use a
  * you are not allowed to modify p
  * only one statement
  * you are not allowed to code anything else than this line of code
  */
-	*(a + 2) = 98;
+	*(p + 5) = 98;
 /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", *(a + 2));
-			return (0);
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
