952cce48718ae7ee3793565e4b1b23772656ca2a
ChalaLe <clelisa28@gmail.com>
2023-01-04 11:36:29 -0800

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,19 @@
 #include "main.h"
 /**
- * _puts_recursion - print a string
+ * _puts_recursion - function that print a string
  * @s:string
  * Return:void
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
 {
 	_putchar('\n');
 }
-	_putchar(*s);
-	_puts_recursion(s + 1);
+
 }
