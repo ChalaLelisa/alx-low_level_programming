db3a4765c76f53feadc4dfbfaf454424c802e247
ChalaLe <clelisa28@gmail.com>
2023-01-06 08:15:15 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,14 @@
 /**
  * main - function that prints its name
  * @argc: argument count
- * @argv: an array of arguments
+ * @argv: arguments
  *
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	printf("%s\n", argv)[0];
+	(void)argc;
+
+	printf("%s\n", argv[0]);
 	return (0);
 }
