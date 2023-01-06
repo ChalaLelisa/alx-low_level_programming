a42e96c523ee4736a65c2da0945fd263421141c8
ChalaLe <clelisa28@gmail.com>
2023-01-06 08:00:24 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints its name
+ * @argc: argument count
+ * @argv: an array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+        printf("%s\n", *argv);
+        return (0);
+}
